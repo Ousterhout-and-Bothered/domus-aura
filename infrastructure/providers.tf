72590175f52394350dcfb9224ddefdb4cfeefe24
emmyatri <aelling6@students.kennesaw.edu>
2026-05-07 21:30:16 -0400

    infra: scaffold terraform with s3 backend and aws provider

---
+++ infrastructure/providers.tf
@@ -0,0 +1,12 @@
+﻿provider "aws" {
+  region  = "us-east-1"
+  profile = "domus-aura-tf"
+
+  default_tags {
+    tags = {
+      Project     = "domus-aura"
+      ManagedBy   = "terraform"
+      Environment = "production"
+    }
+  }
+}
